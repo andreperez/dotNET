1e836b674166de8a3672c71f36138b7d9d862176
André Eduardo Pérez Álvarez <andrebolivia@gmail.com>
2017-08-30 21:23:36 -0300

    Versão inicial

---
+++ EDX/Data Client Applications and Services/edX.DataApp.Console/edX.DataApp.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace edX.DataApp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+            System.Console.WriteLine("Application has completed execution. Press any key to exit.");
+            System.Console.ReadKey();
+        }
+
+        static async Task RunAsync()
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLOCALDB;Initial Catalog=ContosoDB;";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+                System.Console.WriteLine("Connection Successful");
+                //await new DataAdapter().RunLogic(connection);
+            }
+        }
+    }
+}
