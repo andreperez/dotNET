5dc6ad24aed525739cf919fb151eefea5b816f3d
André Eduardo Perez Álvarez <andrebolivia@gmail.com>
2017-09-10 00:28:12 -0300

    10/09/2017

---
+++ EDX/Data Client Applications and Services/edX.DataApp.CoreConsole/edX.DataApp.CoreConsole/ProductAndCategoryQuery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace edX.DataApp.CoreConsole
+{
+    public class ProductAndCategoryQuery
+    {
+
+        public async Task RunLogic(ContosoContext context)
+        {
+            IEnumerable<Product> products = await context.Products
+                .Include(p => p.ProductCategory)
+                .Where(p => p.ListPrice > 1250m && p.ListPrice < 1450m)
+                .Take(20)
+                .ToListAsync();
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"[{product.ProductCategory.Name}]\t{product.Name,35}\t{product.ListPrice:C}");
+            }
+        }
+
+    }
+}
